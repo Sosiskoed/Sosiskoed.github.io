7185e3e903c2f6dd31cc7e49a215ac00bbdb942f
Sosiskoed <sosiskoed2013@yandex.ru>
2019-12-17 22:18:15 +0300

    feat: add swiper and projects block

--- lession_22/base/js/main.js
+++ lession_22/base/js/main.js
@@ -1,21 +1,3 @@
-/*
-document.addEventListener("DOMContentLoaded", function(event) { 
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal');
-  const closeBtn = document.querySelector('.modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
-
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
-  });
-  
-  closeBtn.addEventListener('click', switchModal);
-
-});
-*/
-
 $(document).ready(function () {
   var modal = $('.modal'),
       modalBtn = $('[data-toggle=modal]'),
@@ -27,4 +9,25 @@ $(document).ready(function () {
   closeBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
+
+  //Слайдер
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 10 + bullets.width() + 10)
+  bullets.css('left', prev.width() + 10)
+
 });
