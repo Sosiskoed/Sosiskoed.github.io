0ad779cb74067743fe0d741b2ef677553838adac
Sosiskoed <sosiskoed2013@yandex.ru>
2019-12-19 22:24:25 +0300

    feat: made button for scrolling to top

--- lession_21/hard/js/main.js
+++ lession_21/hard/js/main.js
@@ -1,21 +1,3 @@
-/*
-document.addEventListener("DOMContentLoaded", function(event) { 
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal');
-  const closeBtn = document.querySelector('.modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
-
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
-  });
-  
-  closeBtn.addEventListener('click', switchModal);
-
-});
-*/
-
 $(document).ready(function () {
   var modal = $('.modal'),
       modalBtn = $('[data-toggle=modal]'),
@@ -27,4 +9,20 @@ $(document).ready(function () {
   closeBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
+});
+
+//Code for button to top
+var btnToTop = $('#to-top');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btnToTop.addClass('show');
+  } else {
+    btnToTop.removeClass('show');
+  }
+});
+
+btnToTop.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
 });
