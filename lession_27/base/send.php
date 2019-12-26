429251d0408a835f4cd8ecabf6fa5c45f9317867
Sosiskoed <sosiskoed2013@yandex.ru>
2019-12-26 21:51:55 +0300

    Made ajax verification

--- lession_27/base/send.php
+++ lession_27/base/send.php
@@ -18,13 +18,13 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'pabuka222@gmail.com';                     // SMTP username
+    $mail->Username   = 'sosiskoed2013@gmail.com';                     // SMTP username
     $mail->Password   = 'rfvtyysq1';                               // SMTP password
     $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 465;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('pabuka222@gmail.com', 'Ярослав');
+    $mail->setFrom('sosiskoed2013@gmail.com', 'Ярослав');
     $mail->addAddress('sosiskoed2013@yandex.ru');     // Add a recipient
 
     // Content
@@ -32,8 +32,13 @@ try {
     $mail->Subject = 'Новая заявка с сайта';
     $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}, его почта: ${userEmail}, его вопрос: ${userQuestion}";
 
-    $mail->send();  
-    header('Location: thanks.html');
+    if ($mail->send()) {
+        echo "ok";
+    } else {
+        echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+    }
+    
+    echo "Форма успешно отправлена";
 } catch (Exception $e) {
     echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
 }
